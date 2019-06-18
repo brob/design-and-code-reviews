f40bd1be1b59e81593b593f25e574446a4201176
James Quick <james.q.quick@outlook.com>
2019-06-18 18:56:48 -0500

    Added minimal validation for email, url, and type

--- functions/submit.js
+++ functions/submit.js
@@ -1,6 +1,7 @@
 const querystring = require('querystring');
 const Trello = require('trello');
 var trello = new Trello(process.env.TRELLO_KEY, process.env.TRELLO_TOKEN);
+var validator = require('validator');
 
 function fromBase64( encodedValue ) {
   return( Buffer.from( encodedValue, "base64" ).toString( "utf8" ));
@@ -14,6 +15,32 @@ exports.handler = (event, context, callback) => {
 
   const parsedBody = JSON.parse(body);
   console.log(parsedBody);
+
+  //Validate inputs
+  if(!validator.isEmail(parsedBody.email)){
+    console.log(`Email is invalid: '${parsedBody.email}'`);
+    return callback(null, {
+      statusCode: 400,
+      body: "Email is invalid"
+    })
+  }
+
+  if(validator.isEmpty(parsedBody.type)){
+    console.log(`Review type cannot be empty`);
+    return callback(null, {
+      statusCode: 400,
+      body: "Review type cannot be empty"
+    })
+  }
+
+  if(!validator.isURL(parsedBody.url)){
+    console.log(`URL is invalid: '${parsedBody.url}'`);
+    return callback(null, {
+      statusCode: 400,
+      body: "URL is invalid"
+    })
+  }
+
   const listID = '5d03f4f06dc7f6384304ee9d';
   const cardName = `${parsedBody.type} Request: ${parsedBody.url}`;
   const cardDesc = `Requested by: ${parsedBody.name ? parsedBody.name : "Not Provided"}
@@ -33,7 +60,7 @@ exports.handler = (event, context, callback) => {
       //?trello API returns a string error message (not an error) if the key is invalid
       //?for now, we can check to see if trelloCard is of type object
       else if(typeof trelloCard !== 'object') {
-        console.log("Could not add card, but trello did not return an error");
+        console.log(`Could not add card, but trello did not return an error: '${trelloCard}'`);
         return callback(null, {
           statusCode: 500,
           body: "Could not add card"
